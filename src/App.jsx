55f5e8dda5f1f87815e5594e6c7d74f704a8ccb3
aagarw56 <aagarw56@ucsc.edu>
2025-11-21 14:27:20 -0800

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello Aanya!</h1>
+      <p>Your React project is set up and ready to build 🎉</p>
+    </div>
+  );
+}
+
