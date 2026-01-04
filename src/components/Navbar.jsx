45d174628641e768392cba116fe37db3f6395e45
iarches <iarches@ucsc.edu>
2026-01-03 17:36:37 -0800

    Add ./src/components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <span className="logo">❤️</span>
+        <h1>Women’s Health Advancement Network</h1>
+      </div>
+
+      <div className="nav-right">
+        <a href="#">Health Topics</a>
+        <a href="#">Articles</a>
+        <a href="#">Resources</a>
+        <a href="#">About</a>
+        <button className="nav-btn">Contact Us</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
