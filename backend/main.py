39ea015bc5e16bf006f893809fe85baccdb1da3d
kaylanachuan <kaylana.chuan@gmail.com>
2026-01-09 17:05:59 -0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
 from auth import auth
-
 from newsletter import newsletter
-from google_api import google_api
+from maps import maps
 
 
 app = Flask(__name__)
@@ -12,7 +11,7 @@ CORS(app)
 
 app.register_blueprint(newsletter, url_prefix="/api")
 app.register_blueprint(auth, url_prefix="/auth")
-app.register_blueprint(google_api, url_prefix="/api")
+app.register_blueprint(maps, url_prefix="/maps")
 
 @app.route('/')
 @app.route('/index')
@@ -22,4 +21,5 @@ def index():
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=8000)
+
 
